395f2060592996b6cc254a09bebfbf19dba38382
EUGENE OSEI SABIE <sabieeugeneosei@yahoo.com>
2026-01-26 14:38:31 +0000

    Dashboard error

--- app/Http/Controllers/Employer/DashboardController.php
+++ app/Http/Controllers/Employer/DashboardController.php
@@ -28,7 +28,7 @@ class DashboardController extends Controller
             ->take(5)
             ->get();
 
-        return view('employer.dashboard', compact(
+        return view('Employer.dashboard', compact(
             'totalJobs',
             'activeJobs',
             'applicationsReceived',
